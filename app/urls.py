0a6fd2cb23ae6a1f58c38bd22e231706b1eff625
nikhil-patil007 <nikhilpatil3701@gmail.com>
2023-08-14 00:38:58 +0530

    categories_nikhil: Create an API for the List of images,categories,resolutions and Design the admin panel.

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home_page_api,),
-    path('Main_category_api/',views.Main_category_api,)
+    path('',views.home_page_api),
+    path('main_category_api/',views.Main_category_api),
+    path('get_all_resolutions/',views.Get_all_resolutions),
+    path('get_sub_categories/<int:getId>/',views.get_sub_categories),
+    path('list_of_images_based_on_category/<int:subCategory>/',views.list_of_images_based_on_category),
 ]
--- app/views.py
+++ app/views.py
@@ -3,8 +3,22 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import *
+from .serializers import *
 
 # Create your views here.
+
+def List_for_data_store(values):
+    List_of_data = list()
+    for arr in values:
+        array_object = dict()
+        array_object["id"] = arr.id
+        array_object["type_of"] = arr.type_of
+        array_object["parent_category"] = arr.parent_category.name
+        array_object["File"] = arr.file_url
+        array_object["Recommended"] = arr.Recommended
+        List_of_data.append(array_object)
+
+    return List_of_data
 
 @api_view(["GET"])
 def home_page_api(request):
@@ -12,22 +26,56 @@ def home_page_api(request):
      API For the Home Page to displayed the all images and Files.
     '''
     recommended_image = DataStorage.objects.filter(Recommended="Yes")
-    List_of_image = []
-    for arr in recommended_image:
-        array_object = {}
-        array_object['id'] = arr.id
-        array_object['type_of'] = arr.type_of
-        array_object['parent_category'] = arr.parent_category.name
-        array_object['File'] = arr.file_url
-        array_object['Recommended'] = arr.Recommended
-        List_of_image.append(array_object)
-        
-    return Response({'status':1,"status_Msg": "Successful","data":List_of_image} ,status=status.HTTP_200_OK)
+    List_of_image = List_for_data_store(recommended_image)
+    return Response({"status_code":1,"status_Msg": "Successful","data":List_of_image} ,status=status.HTTP_200_OK)
 
 @api_view(['GET'])
+def Get_all_resolutions(request):
+    '''
+    Get the List of All Resolutions
+    '''
+    getAllResolutions = Resolutions.objects.all()
+    List_of_resolutions = ResolutionSerializer(getAllResolutions,many=True)
+    return Response({"status_code":1,"status_Msg": "Successful","data":List_of_resolutions.data} ,status=status.HTTP_200_OK)
+
+@api_view(["GET"])
 def Main_category_api(request):
     '''
     API for the getting List of main category.
+    if Param_value is pass with url like of resolutions data that also filter by resolutions type of data.
+
+    url will pass like this for the param value 
+    example: Main_category_api/?param_value=1
     '''
-    allCategories = Categories.objects.get(parent_category=None)
-    return Response({'status':1,"status_Msg": "Successful","data":List_of_image} ,status=status.HTTP_200_OK)
+    param_value = request.GET.get("param_value", 0)
+    if param_value:
+        try:
+            resolution_data = Resolutions.objects.get(id=param_value)
+            allCategories = Categories.objects.filter(parent_category=None,Resolution=resolution_data)
+        except:
+            # allCategories = Categories.objects.filter(parent_category=None)
+            return Response({"status_code":0,"status_Msg":"Please Select valid Category"},status=status.HTTP_400_BAD_REQUEST)
+    else:
+        allCategories = Categories.objects.filter(parent_category=None)
+
+    List_of_main_category = CategorySerializer(allCategories,many=True)
+    return Response({"status_code":1,"status_Msg": "Successful","data":List_of_main_category.data} ,status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def get_sub_categories(request,getId):
+    '''
+    Get Sub Categories Based on main Categories Id pass with url
+    '''
+    allSubCategory = Categories.objects.filter(parent_category=getId)
+    List_of_sub_category = CategorySerializer(allSubCategory,many=True)
+    return Response({"status_code":1,"status_Msg": "Successful","data":List_of_sub_category.data} ,status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+def list_of_images_based_on_category(request,subCategory):
+    '''
+    Get the List of Images and files based on the category
+    '''
+    recommended_image = DataStorage.objects.filter(parent_category=subCategory)
+    List_of_image = List_for_data_store(recommended_image)
+    return Response({"status_code":1,"status_Msg": "Successful","data":List_of_image} ,status=status.HTTP_200_OK)
